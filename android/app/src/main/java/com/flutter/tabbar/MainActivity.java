a5596a5e12610dcf7926c834ff48976e37e3ce33
Fachmieee <fachmieeee@gmail.com>
2021-06-06 20:00:24 +0700

    Initial commit

---
+++ android/app/src/main/java/com/flutter/tabbar/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flutter.tabbar;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Hi!"),
+            bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    icon: Icon(Icons.comment), text: "Comment",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.image), text: "Image",
+                  ),
+                ]),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Text("Tab 1"),
+              ),
+              Center(
+                child: Text("Tab 2"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
